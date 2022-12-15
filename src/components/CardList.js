1b234b9997ffd04ed51cf989a7d139e74aea7e73
AlexandreBIEN <91879953+AlexandreBIEN@users.noreply.github.com>
2022-12-15 13:34:10 +0100

    pagination + css boutno pagination

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -9,8 +9,12 @@ const CardList = () => {
   // Information des personnages
   const[characters, setCharacters] = useState([]);
 
-  useEffect(() => {
-    axios.get('https://rickandmortyapi.com/api/character')
+  // numéro de la page demandé
+  const[page, setPage] = useState(1);
+
+  // Requête vers l'api qui récupère les 20 premier personnage
+  async function getCharacter(page) {
+    axios.get('https://rickandmortyapi.com/api/character?page=' + page)
     .then(function (response) {
       // On renseigne les informations des personnages dans une variable
       setCharacters(response.data.results);
@@ -18,14 +22,51 @@ const CardList = () => {
     .catch(function (error) {
       console.log(error);
     })
-  }, []);
+  }
+
+
+  useEffect(() => {
+    getCharacter(page)
+  }, [page]);
+
+  // Fonction pour aller à la page précédente
+  const handlePrevious = () => {
+    if(page != 1){
+      let PrevPage = page - 1;
+      setPage(PrevPage);
+    }
+  }
+
+  // Fonction pour aller à la page suivante
+  const handleNext = () => {
+      let nextPage = page + 1;
+      setPage(nextPage); 
+  }
 
   return (
-      <main className='cards'>
+    <main>
+      {/* Bouton pagination */}
+      <div className='pagination'>
+        <button className='card-btn' onClick={() => handlePrevious(page)}>Précédent</button>
+        <p className='text-white'>Page {page}</p>
+        <button className='card-btn' onClick={() => handleNext(page)}>Suivant</button>
+      </div>
+      {/* Liste des personnages */}
+      <div className='cards'>
         {characters.map((character) => {
           return <Card name={character.name} key={character.id} id={character.id} location={character.location.name} image={character.image} status={character.status}/>
         })}
-      </main>
+        
+      </div>
+      {/* Bouton pagination */}
+      <div className='pagination'>
+        <button className='card-btn' onClick={() => handlePrevious(page)}>Précédent</button>
+        <p className='text-white'>Page {page}</p>
+        <button className='card-btn' onClick={() => handleNext(page)}>Suivant</button>
+      </div>
+      
+    </main>
+      
   )
 }
 
